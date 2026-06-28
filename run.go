01cb2ea9394cbc2947e0ec7712a330935d5780dd
Robert Watkin <robert.watkin12@gmail.com>
2026-06-28 01:20:44 +0100

    feat: identify files with duplicate sizes

--- run.go
+++ run.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
 	"io"
-	"io/fs"
-	"path/filepath"
+	"slices"
 )
 
 type fileDets struct {
@@ -14,47 +14,40 @@ type fileDets struct {
 
 // handles main running of the program and any error handling
 func run(opts []string, in io.Reader, out io.Writer, stderr io.Writer) (int, error) {
-	allFiles, err := walk(opts[0], stderr)
+	// walk tree - gather files and sizes
+	unsortedFiles, err := walk(opts[0], stderr)
 	if err != nil {
 		return 1, fmt.Errorf("Error: A problem has occurred during the run function %w", err)
 	}
 
-	for _, file := range allFiles {
-		fmt.Println(file.name, file.size)
+	// count sizes
+	counts := make(map[int64]int)
+	for _, file := range unsortedFiles {
+		counts[file.size]++
 	}
+
+	// return only files where the size matches with other files
+	var sortedFiles []fileDets
+	filesPerGroup := make(map[int64]int)
+	for _, file := range unsortedFiles {
+		if counts[file.size] >= 2 {
+			sortedFiles = append(sortedFiles, file)
+			filesPerGroup[file.size]++
+		}
+	}
+
+	// order based on size - not necessary for calculation but convenient for debugging loop
+	slices.SortFunc(sortedFiles, func(a, b fileDets) int {
+		return cmp.Compare(a.size, b.size)
+	})
+
+	// debugging loop
+	for _, file := range sortedFiles {
+		fmt.Fprintln(stderr, file.name, file.size)
+	}
+
+	// stderr debug log
+	fmt.Fprintf(stderr, "%v files scanned, %v candidates in %v size groups\n", len(unsortedFiles), len(sortedFiles), len(filesPerGroup))
 
 	return 0, nil
-}
-
-func walk(rootName string, stderr io.Writer) ([]fileDets, error) {
-	var allFiles []fileDets
-
-	err := filepath.WalkDir(rootName, func(path string, d fs.DirEntry, err error) error {
-		// warning when the file is unreadable
-		if err != nil {
-			fmt.Fprintln(stderr, "WARNING: File is unreadable -", err)
-			return nil
-
-		}
-
-		// non-regular file gets skipped
-		if !d.Type().IsRegular() {
-			return nil
-		}
-
-		// get file info
-		info, err := d.Info()
-		if err != nil {
-			return err
-		}
-
-		allFiles = append(allFiles, fileDets{path, info.Size()})
-
-		return nil
-	})
-	if err != nil {
-		return allFiles, err
-	}
-
-	return allFiles, nil
 }
---
+++ walk.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/fs"
+	"path/filepath"
+)
+
+func walk(rootName string, stderr io.Writer) ([]fileDets, error) {
+	var allFiles []fileDets
+
+	err := filepath.WalkDir(rootName, func(path string, d fs.DirEntry, err error) error {
+		// warning when the file is unreadable
+		if err != nil {
+			fmt.Fprintln(stderr, "WARNING: File is unreadable -", err)
+			return nil
+
+		}
+
+		// non-regular file gets skipped
+		if !d.Type().IsRegular() {
+			return nil
+		}
+
+		// get file info
+		info, err := d.Info()
+		if err != nil {
+			return err
+		}
+
+		allFiles = append(allFiles, fileDets{path, info.Size()})
+
+		return nil
+	})
+	if err != nil {
+		return allFiles, err
+	}
+
+	return allFiles, nil
+}
